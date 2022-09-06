72e02be0cada8ae15c16e47f3bfc56e6a13930a9
austinmetcalf <austinchaz11@gmail.com>
2022-09-06 11:12:59 -0600

    add comments

---
+++ index.js
@@ -0,0 +1,1 @@
+// steps: set up schema/ tables, create seeds, establish connection to mysql database. Create inquirer application that takes in user input and allows view departments employees and roles.  Add departments, employees, and roles.  Update employee roles.
